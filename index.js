14c39fdcde7d28d7e44c082a4e8998330c2bb1e6
HusseinSerag <117987162+HusseinSerag@users.noreply.github.com>
2023-10-20 16:32:24 +0300

    Handle two operator additions and most of errors
    code now handles operations well (add , divide , multi , sub) , with
    some errors solved

--- index.js
+++ index.js
@@ -9,16 +9,9 @@ function operate(operand1 , operand2 , operator){
 }
 let operand1 = ''
 let operand2 = ''
-let operation;
+let operation = ''
 let operations = ['*' , '/' , '+' , '-' , '^']
-function determineOperands(operand , operator){
-    if(operator == undefined){
-        operand1 += operand
-    }
-    else{
-        operand2 += operand
-    }
-}
+
 keys.addEventListener('click' , e=>{
     e.stopPropagation()
     let TARGET  = e.target.id
@@ -37,17 +30,74 @@ keys.addEventListener('click' , e=>{
         screen.textContent = content
     }
     else if(TARGET == 'equal'){
-        operate(operand1 , operand2 , operation)
+        determineOps(screen.textContent)
+        screen.textContent = operate(operand1 , operand2 , operation)
+        
+        operand1 = ''
+        operand2 = ''
+        operation = ''
+        
+    }
+    else if(TARGET == '+' || TARGET == '-' || TARGET == '*' || TARGET == '/'){
+        console.log(operation.length)
+        
+            screen.textContent += TARGET
+        operation += TARGET
+        
+
     }
     else{
-        let value = operations.find(operation1 => operation1 == operation)
-        console.log(value)
-        determineOperands(TARGET , operation)
         screen.textContent += TARGET
     }
-   
+    
 
-console.log(operand1)
-console.log(operand2)
 
 })
+
+function determineOps(string){
+
+   const re = /[+*-/]/
+    stringArr = string.split(re)
+    operand1 = stringArr[0]
+    operand2 = stringArr[1]
+    console.log(operand1)
+    console.log(operand2)
+    
+
+}
+function operate(op1,op2,operator){
+    if(operator.length != 1){
+        return"ERR"
+        
+    }
+    if(operand1 == '' || operand2 == ''){
+        return "ERR"
+        
+    }
+    if(operator == '+'){
+        return add(op1 , op2)
+    }
+    else if(operator == '-'){
+        return subtract(op1,op2)
+    }
+    else if(operator == '*'){
+        return multiply(op1,op2)
+    }
+    else if(operator == '/'){
+        return divide(op1,op2)
+    }
+}
+
+function add(a,b){
+    
+    return Number.parseInt(a) + Number.parseInt(b)
+}
+function subtract(a,b){
+    return a-b
+}
+function multiply(a,b){
+    return a*b
+}
+function divide(a,b){
+    return a/b
+}
