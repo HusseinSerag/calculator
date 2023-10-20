4c6525caad6f5582d7e07474aee6ec16c1d90966
HusseinSerag <117987162+HusseinSerag@users.noreply.github.com>
2023-10-20 18:11:50 +0300

    Implement All functionality with error parsing errors are handled compelety and functionality is handed

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 
 const keys = document.getElementById('keys')
-const screen = document.getElementById('screen')
+const screen = document.getElementById('main')
+const equals = document.getElementById('equals')
 
 function operate(operand1 , operand2 , operator){
-    if(operate == "+"){
-        Number.parseInt(operand1) + Number.parseInt(operand2)
+    if(operator == "+"){
+        return add(operand1, operand2)
+    }
+    else if(operator == "-"){
+        return subtract(operand1, operand2)
+    }
+    else if(operator == "*"){
+        return multiply(operand1, operand2)
+    }
+    else if(operator == '/'){
+        return divide(operand1 , operand2)
+    }
+    else if(operator == "^"){
+        return pow(operand1, operand2)
     }
 }
 let operand1 = ''
@@ -20,6 +33,7 @@ keys.addEventListener('click' , e=>{
         operand2 = ''
         operation = ''
         screen.textContent = ''
+        equals.textContent = ''
     }
     else if(TARGET == 'keys'){
 
@@ -30,42 +44,76 @@ keys.addEventListener('click' , e=>{
         screen.textContent = content
     }
     else if(TARGET == 'equal'){
-        determineOps(screen.textContent)
-        screen.textContent = operate(operand1 , operand2 , operation)
-        
-        operand1 = ''
-        operand2 = ''
-        operation = ''
+        equalFunction()
         
     }
-    else if(TARGET == '+' || TARGET == '-' || TARGET == '*' || TARGET == '/'){
-        console.log(operation.length)
+    else if(TARGET == '+' || TARGET == '-' || TARGET == '*' || TARGET == '/' || TARGET == '^'){
+        if(equals.textContent != ''){
+            operand1 = equals.textContent
+            screen.textContent = "ans"
+            
+        }
+        screen.textContent += TARGET
         
-            screen.textContent += TARGET
-        operation += TARGET
+        
         
 
     }
+   
     else{
         screen.textContent += TARGET
+        if(equals.textContent != ''){
+            equalFunction()
+        }
     }
     
 
 
 })
-
+function equalFunction(){
+    determineOps(screen.textContent)
+    number = (calc(operand1 , operand2 , operation))
+    if(Number.isInteger(number)){
+        equals.textContent = number
+    }else{
+        equals.textContent = Number.parseFloat(number).toFixed(3)
+    }
+        
+        screen.textContent = ''
+        operand1 = ''
+        operand2 = ''
+        operation = ''
+}
 function determineOps(string){
 
-   const re = /[+*-/]/
-    stringArr = string.split(re)
+   const re =  /[\+\*\-\/\^]/
+   for(let i = 0 ; i < string.length ; i++){
+    if((string[i] == '+' || string[i] == '-' || string[i] == '*' || string[i] == '/' || string[i] == '^')){
+        operation += string[i]
+        
+    }
+   }
+   
+    let stringArr = string.split(re)
+    if(operand1 == '')
     operand1 = stringArr[0]
+
+
     operand2 = stringArr[1]
+    console.log(stringArr)
     console.log(operand1)
     console.log(operand2)
     
 
 }
-function operate(op1,op2,operator){
+
+
+
+
+
+
+
+function calc(op1,op2,operator){
     if(operator.length != 1){
         return"ERR"
         
@@ -74,23 +122,26 @@ function operate(op1,op2,operator){
         return "ERR"
         
     }
-    if(operator == '+'){
-        return add(op1 , op2)
-    }
-    else if(operator == '-'){
-        return subtract(op1,op2)
-    }
-    else if(operator == '*'){
-        return multiply(op1,op2)
-    }
-    else if(operator == '/'){
-        return divide(op1,op2)
-    }
+    return operate(op1 , op2 , operator)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function add(a,b){
     
-    return Number.parseInt(a) + Number.parseInt(b)
+    return Number.parseFloat(a) + Number.parseFloat(b)
 }
 function subtract(a,b){
     return a-b
@@ -99,5 +150,8 @@ function multiply(a,b){
     return a*b
 }
 function divide(a,b){
-    return a/b
+    return (a/b)
+}
+function pow(a,b){
+    return a ** b
 }
